9550fbb91d8f05a480cdcc5bb84580a62ce6b1e0
Oleks Oleks <shpakovsky94@gmail.com>
2020-07-13 21:16:15 +0300

    First commit

---
+++ 02-backend/spring-boot-checkify/src/main/java/com/finalProject/checkify/SpringBootCheckifyApplication.java
@@ -0,0 +1,13 @@
+package com.finalProject.checkify;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootCheckifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootCheckifyApplication.class, args);
+	}
+
+}
---
+++ 02-backend/spring-boot-checkify/src/test/java/com/finalProject/checkify/SpringBootCheckifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.finalProject.checkify;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootCheckifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

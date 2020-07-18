33b3918d43fa5ba6f682ab03f451a46969bce60c
TriinRebane <55159195+TriinRebane@users.noreply.github.com>
2020-07-18 23:19:30 +0300

    all the entities

--- 02-backend/spring-boot-checkify/src/main/java/com/finalProject/checkify/entity/Product.java
+++ 02-backend/spring-boot-checkify/src/main/java/com/finalProject/checkify/entity/Product.java
@@ -1,4 +1,41 @@
 package com.finalProject.checkify.entity;
 
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Entity
+@Data
+@Table(name = "PRODUCT")
 public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "BARCODE", length = 20)
+    private String barcode;
+
+    @Column(name = "NAME", length = 150)
+    private String name;
+
+    @Column(name = "EXPIRE_DATE")
+    private Date date;
+
+    @Column(name = "DESCRIPTION")
+    private String description;
+
+    @Column(name = "IMAGE_URL")
+    private String imageUrl;
+
+    @ManyToOne
+    @JoinColumn(name = "CATEGORY_ID")
+    private  ProductCategory productCategory;
+
+    @ManyToOne
+    @JoinColumn(name = "FRIDGE_ID")
+    private  Fridge fridge;
+
 }

4f8a9d3e328faa12592caa15f68ae0d4b2f74aca
TriinRebane <55159195+TriinRebane@users.noreply.github.com>
2020-07-31 22:26:28 +0300

    third commit pagination- page select and search by keyword updated with pagination

--- 03-frontend/angular-checkify/src/app/components/product-list/product-list.component.ts
+++ 03-frontend/angular-checkify/src/app/components/product-list/product-list.component.ts
@@ -17,9 +17,10 @@ export class ProductListComponent implements OnInit {
 
     // new properties for pagination
     thePageNumber: number = 1;
-    thePageSize: number = 10;
+    thePageSize: number = 5;
     theTotalElements: number = 0;
     
+    previousKeyword: string = null;
 
   constructor(private productService: ProductService,
               private route: ActivatedRoute) { }
@@ -43,14 +44,25 @@ export class ProductListComponent implements OnInit {
   }
   
   handleSearchProducts() {
+
     const theKeyword: string = this.route.snapshot.paramMap.get('keyword');
 
+    // if we have a different keyword than previous
+    // then set thePageNumber to 1
+
+    if (this.previousKeyword != theKeyword) {
+      this.thePageNumber = 1;
+    }
+
+    this.previousKeyword = theKeyword;
+
+    console.log(`keyword=${theKeyword}, thePageNumber=${this.thePageNumber}`);
+
     // now search for the products using keyword
-    this.productService.searchProducts(theKeyword).subscribe(
-      data => {
-        this.products = data;
-      }
-    )
+    this.productService.searchProductsPaginate(this.thePageNumber - 1,
+                                               this.thePageSize,
+                                               theKeyword).subscribe(this.processResult());
+                                               
   }
 
   // tslint:disable-next-line:typedef
@@ -87,6 +99,12 @@ export class ProductListComponent implements OnInit {
       this.thePageSize = data.page.size;
       this.theTotalElements = data.page.totalElements;
     };
+  }
+
+  updatePageSize(pageSize: number) {
+    this.thePageSize = pageSize;
+    this.thePageNumber = 1;
+    this.listProducts();
   }
 
   deleteFromFridge(id: string) {
